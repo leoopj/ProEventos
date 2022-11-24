ef327dcf5e0c34a9e9c305483f3770536e98703f
Leonardo Pereira de Jesus <leonardo.pereira@keeggo.com>
2022-11-23 22:49:43 -0300

    Seção 4 - Angular

---
+++ Web/ProEvento-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos', //define nome para instanciar componente
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void
+  {
+    //chamada de API
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+
+  }
+
+}
---
+++ Web/ProEvento-App/src/app/palestrantes/palestrantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-palestrantes', //define nome para instanciar componente
+  templateUrl: './palestrantes.component.html',
+  styleUrls: ['./palestrantes.component.scss']
+})
+export class PalestrantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
