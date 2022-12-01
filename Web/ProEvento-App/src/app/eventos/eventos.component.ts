9e07b24cf7227df2a7e6ff00fa10e3e01370c60f
Leonardo Pereira de Jesus <leonardo.pereira@keeggo.com>
2022-11-30 22:28:02 -0300

    Seção 5 v2: Interpolação, Diretivas e Biding

--- Web/ProEvento-App/src/app/eventos/eventos.component.ts
+++ Web/ProEvento-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 50;
+  margimImg: number = 2;
+  viewImage: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string
+  {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string)
+  {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtraEventos(this.filtroLista) : this.eventos
+  }
+
+  filtraEventos(filtraPor: string): any
+  {
+    filtraPor = filtraPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtraPor) !== -1
+      || evento.local.toLocaleLowerCase().indexOf(filtraPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,11 +41,19 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterViewImage(){
+    this.viewImage = !this.viewImage;
+  }
+
   public getEventos(): void
   {
     //chamada de API
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response =>
+      {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     );
 
